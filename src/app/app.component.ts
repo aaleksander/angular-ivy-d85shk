9dec66810dc2c0a847932c2e021a148293ffcced
aaleksander <aleksandranufriev@yandex.ru>
2021-09-29 11:22:00 +0300

    вывод списка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Character } from './dao/character';
 import { RicoService } from './services/ricoService';
 
 @Component({
@@ -8,15 +9,24 @@ import { RicoService } from './services/ricoService';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  res: string;
+  res: Character[];
 
   constructor(private http: RicoService) {}
 
   test() {
     this.http.get('https://rickandmortyapi.com/api/character/').subscribe(
-      (data) => (this.res = JSON.stringify(data.results)),
+      (response) => {
+        console.info('start');
+        this.res = [];
+        for (let item of response.results) {
+          let p = new Character(item.id, item.name, item.status);
+          this.res.push(p);
+        }
+        console.info('end');
+        //this.res = JSON.stringify(data.results);
+      },
       (error) => {
-        this.res = JSON.stringify(error);
+        //this.res = JSON.stringify(error);
       }
     );
   }
